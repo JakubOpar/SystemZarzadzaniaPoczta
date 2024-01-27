fbd68d03831f1c97ac4fba1573cb4d9ad463a4ed
Jakub Opar <jakubopar@gmail.com>
2024-01-27 20:17:41 +0100

    poprawnienie klasy NadawcaIOdbiorca

--- PROJEKT/Projekt Programowanie Obiektowe/src/NadawcaIOdbiorca.java
+++ PROJEKT/Projekt Programowanie Obiektowe/src/NadawcaIOdbiorca.java
@@ -26,80 +26,31 @@ public class NadawcaIOdbiorca {
     public String getImieNadawcy() {
         return imieNadawcy;
     }
-
-    public void setImieNadawcy(String imieNadawcy) {
-        this.imieNadawcy = imieNadawcy;
-    }
-
     public String getNazwiskoNadawcy() {
         return NazwiskoNadawcy;
     }
-
-    public void setNazwiskoNadawcy(String nazwiskoNadawcy) {
-        NazwiskoNadawcy = nazwiskoNadawcy;
-    }
-
     public String getAdresNadawcy() {
         return AdresNadawcy;
     }
-
-    public void setAdresNadawcy(String adresNadawcy) {
-        AdresNadawcy = adresNadawcy;
-    }
-
     public String getKodPocztowyNadawcy() {
         return KodPocztowyNadawcy;
     }
-
-    public void setKodPocztowyNadawcy(String kodPocztowyNadawcy) {
-        KodPocztowyNadawcy = kodPocztowyNadawcy;
-    }
-
     public String getKpMiejscowoscNadawcy() {
         return kpMiejscowoscNadawcy;
     }
-
-    public void setKpMiejscowoscNadawcy(String kpMiejscowoscNadawcy) {
-        this.kpMiejscowoscNadawcy = kpMiejscowoscNadawcy;
-    }
-
     public String getImieObdiorcy() {
         return imieObdiorcy;
     }
-
-    public void setImieObdiorcy(String imieObdiorcy) {
-        this.imieObdiorcy = imieObdiorcy;
-    }
-
     public String getNazwiskoOdbiorcy() {
         return nazwiskoOdbiorcy;
     }
-
-    public void setNazwiskoOdbiorcy(String nazwiskoOdbiorcy) {
-        this.nazwiskoOdbiorcy = nazwiskoOdbiorcy;
-    }
-
     public String getAdresObdiorcy() {
         return AdresObdiorcy;
     }
-
-    public void setAdresObdiorcy(String adresObdiorcy) {
-        AdresObdiorcy = adresObdiorcy;
-    }
-
     public String getKodPocztowyOdbiorcy() {
         return KodPocztowyOdbiorcy;
     }
-
-    public void setKodPocztowyOdbiorcy(String kodPocztowyOdbiorcy) {
-        KodPocztowyOdbiorcy = kodPocztowyOdbiorcy;
-    }
-
     public String getKpMiejscowoscOdbiorcy() {
         return kpMiejscowoscOdbiorcy;
-    }
-
-    public void setKpMiejscowoscOdbiorcy(String kpMiejscowoscOdbiorcy) {
-        this.kpMiejscowoscOdbiorcy = kpMiejscowoscOdbiorcy;
     }
 }
